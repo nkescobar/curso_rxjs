4d4c4eeadfcb2b320b6749484f99d1effd8ad266
Nasly Katherine Escobar <nkescobar94@gmail.com>
2022-06-24 11:07:23 -0500

    Creacion de observable range

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { of, range } from 'rxjs';
 
-/**Crea un Observable que emite una secuencia
- *  de números dentro de un rango específico.
- * Emite una secuencia de números en un rango.
- */
-
-const src$ = of (1,2,3,4,5);
-const rango$ = range(1,10);
-
-console.log('Inicio');
-src$.subscribe(console.log);
-console.log('fin');
-
-console.log('Inicio rangce');
-rango$.subscribe(console.log);
-console.log('fin range');
